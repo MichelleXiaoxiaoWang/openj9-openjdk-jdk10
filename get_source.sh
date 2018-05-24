740583f15853431231ab77b61ec2d9b1efbfd995
Xiaoxiao Wang <xiaoxiao.wang@ibm.com@xiaoxiaos-mbp.ottriv.can.ibm.com>
2018-05-24 10:20:46 -0400

    new branch Signed-off-by: Xiaoxiao Wang <xiaoxiao.wang@ibm.com>

--- get_source.sh
+++ get_source.sh
@@ -3,11 +3,11 @@
 # (c) Copyright IBM Corp. 2017, 2018 All Rights Reserved
 # ===========================================================================
 #
-# Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2018, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
-# add content for test
+# change content for test
 # under the terms of the GNU General Public License version 2 only, as
 # published by the Free Software Foundation.  Oracle designates this
 # particular file as subject to the "Classpath" exception as provided
